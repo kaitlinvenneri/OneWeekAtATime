b358f27de41a6672d53d754aa973ce92d0bdc9de
Kaitlin <vennerik@uoguelph.ca>
2020-04-28 13:06:26 -0400

    Completing more backend requests - can now add, edit, delete tasks. All tasks can also be viewed in json and marked as complete/incomplete.

--- index.js
+++ index.js
@@ -42,17 +42,86 @@
         });
     })
 
-    app.get('/tasks/add', (req, res) => {
+    app.get('/task/add', (req, res) => {
         const {title} = req.query;
-        console.log(title);
 
         const INSERT_TASK_QUERY = `INSERT INTO task(title) VALUES('${title}')`
 
-        connection.query(INSERT_TASK_QUERY, (err, results) => {
+        connection.query(INSERT_TASK_QUERY, (err) => {
             if(err) {
                 return res.send(err)
             } else {
                 return res.send('successfully added task')
+            }
+        });
+    })
+
+    app.get('/task/schedule', (req, res) => {
+        const {id, date} = req.query;
+
+        const INSERT_TASK_QUERY = `UPDATE task SET scheduleddate = '${date}' WHERE taskid = '${id}';`
+
+        connection.query(INSERT_TASK_QUERY, (err) => {
+            if(err) {
+                return res.send(err)
+            } else {
+                return res.send('successfully scheduled task')
+            }
+        });
+    })
+
+    app.get('/task/mark-complete', (req, res) => {
+        const {id} = req.query;
+
+        const INSERT_TASK_QUERY = `UPDATE task SET completionstatus = '1' WHERE taskid = '${id}';`
+
+        connection.query(INSERT_TASK_QUERY, (err) => {
+            if(err) {
+                return res.send(err)
+            } else {
+                return res.send('successfully marked task as complete')
+            }
+        });
+    })
+
+    app.get('/task/mark-incomplete', (req, res) => {
+        const {id} = req.query;
+
+        const INSERT_TASK_QUERY = `UPDATE task SET completionstatus = '0' WHERE taskid = '${id}';`
+
+        connection.query(INSERT_TASK_QUERY, (err) => {
+            if(err) {
+                return res.send(err)
+            } else {
+                return res.send('successfully marked task as incomplete')
+            }
+        });
+    })
+
+    app.get('/task/delete', (req, res) => {
+        const {id} = req.query;
+
+        const INSERT_TASK_QUERY = `DELETE from task WHERE taskid = '${id}';`
+
+        connection.query(INSERT_TASK_QUERY, (err) => {
+            if(err) {
+                return res.send(err)
+            } else {
+                return res.send('successfully deleted task')
+            }
+        });
+    })
+
+    app.get('/task/update-title', (req, res) => {
+        const {id, title} = req.query;
+
+        const INSERT_TASK_QUERY = `UPDATE task SET title = '${title}' WHERE taskid = '${id}';`
+
+        connection.query(INSERT_TASK_QUERY, (err) => {
+            if(err) {
+                return res.send(err)
+            } else {
+                return res.send('successfully updated the title of the task')
             }
         });
     })
