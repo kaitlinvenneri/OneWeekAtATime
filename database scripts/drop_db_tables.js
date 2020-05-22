31f1a2c7c7eb37694c3b4cc447ba19f11c609cb9
Kaitlin <vennerik@uoguelph.ca>
2020-05-22 02:05:33 -0400

    Commenting database scripts

--- database scripts/drop_db_tables.js
+++ database scripts/drop_db_tables.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql');
 
+//Setting the default connection variables for the database (assuming grading flag not provided)
 let hostname = "localhost";
 let username = "root";
 let dbPassword = "password";
 let dbName = "planningApp";
 
+//Modifying database connection variables if server is being run for grading purposes
 const args = process.argv.slice(2)
 if(args[0] === 'grading'){
     hostname = "dursley.socs.uoguelph.ca";
@@ -14,13 +16,14 @@ if(args[0] === 'grading'){
     grading = true;
 }
 
+//Creating connection variable for connecting to database
 var connection = mysql.createConnection({
     host: hostname,
     user: username,
     password: dbPassword
 });
 
-
+//Connecting to the database and completing each query to drop tables
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
--- database scripts/drop_db_with_tables.js
+++ database scripts/drop_db_with_tables.js
@@ -1,11 +1,13 @@
 var mysql = require('mysql');
 
+//Setting the default connection variables for the database (assuming grading flag not provided)
 let hostname = "localhost";
 let username = "root";
 let dbPassword = "password";
 let dbName = "planningApp";
 let grading = false;
 
+//Modifying database connection variables if server is being run for grading purposes
 const args = process.argv.slice(2)
 if(args[0] === 'grading'){
     hostname = "dursley.socs.uoguelph.ca";
@@ -15,13 +17,15 @@ if(args[0] === 'grading'){
     grading = true;
 }
 
+//Creating connection variable for connecting to database
 var connection = mysql.createConnection({
     host: hostname,
     user: username,
     password: dbPassword
 });
 
-
+//Connecting to the database and completing each query to drop database and tables
+//It should be noted that for grading, the database will not be dropped on the socs server
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
