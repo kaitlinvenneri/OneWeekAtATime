54f18adcd3e20196d6b6676363b8547cbb766c9e
Kaitlin <vennerik@uoguelph.ca>
2020-05-22 01:20:27 -0400

    Adding comments

--- server/index.js
+++ server/index.js
@@ -3,17 +3,21 @@
     const cors = require('cors');
     const app = express();
 
+    //cors needed for React integration since frontend exists on its own server
     app.use(cors());
 
+    //server reachable at http://localhost:4000
     app.listen(4000, () => {
         console.log("Listening on port 4000");
     })
 
+    //Setting the default connection variables for the database (assuming grading flag not provided)
     let hostname = "localhost";
     let username = "root";
     let dbPassword = "password";
     let dbName = "planningApp";
 
+    //Modifying database connection variables if server is being run for grading purposes
     const args = process.argv.slice(2)
     if(args[0] === 'grading'){
         hostname = "dursley.socs.uoguelph.ca";
@@ -22,6 +26,7 @@
         dbName = "vennerik";
     }
 
+    //Creating connection variable for connecting to database
     var connection = mysql.createConnection({
         host: hostname,
         user: username,
@@ -29,6 +34,7 @@
         database: dbName
     });
 
+    //Connecting to the database
     connection.connect(err => {
         if (err) {
             throw err;
@@ -37,10 +43,12 @@
         }
     })
 
+    //Endpoint for homepage
     app.get('/', (req, res) => {
         res.send('homepage')
     })
 
+    //Endpoint to view all tasks
     app.get('/tasks', (req, res) => {
         const SELECT_ALL_TASKS_QUERY = `SELECT * FROM Task`
         
@@ -55,6 +63,7 @@
         });
     })
 
+    //Endpoint to view all scheduled tasks
     app.get('/scheduled-tasks', (req, res) => {
         const SELECT_ALL_TASKS_QUERY = `SELECT * FROM ScheduledTask`
 
@@ -69,6 +78,7 @@
         });
     })
 
+    //Endpoint to add a task
     app.get('/task/add', (req, res) => {
         const {title} = req.query;
 
@@ -83,6 +93,7 @@
         });
     })
 
+    //Endpoint to schedule a task
     app.get('/task/schedule', (req, res) => {
         const {id, date} = req.query;
 
@@ -97,6 +108,7 @@
         });
     })
 
+    //Endpoint to mark a task as complete
     app.get('/task/mark-complete', (req, res) => {
         const {id} = req.query;
 
@@ -111,6 +123,7 @@
         });
     })
 
+    //Endpoint to mark a scheduled task as complete
     app.get('/scheduled-task/mark-complete', (req, res) => {
         const {id} = req.query;
 
@@ -125,6 +138,7 @@
         });
     })
 
+    //Endpoint to mark a task as incomplete
     app.get('/task/mark-incomplete', (req, res) => {
         const {id} = req.query;
 
@@ -139,6 +153,7 @@
         });
     })
 
+    //Endpoint to mark a scheduled task as incomplete
     app.get('/scheduled-task/mark-incomplete', (req, res) => {
         const {id} = req.query;
 
@@ -153,6 +168,7 @@
         });
     })
 
+    //Endpoint to delete a task
     app.get('/task/delete', (req, res) => {
         const {id} = req.query;
 
@@ -167,6 +183,7 @@
         });
     })
 
+    //Endpoint to delete a scheduled task (unschedule a task for the date within the scheduled task)
     app.get('/scheduled-task/delete', (req, res) => {
         const {id} = req.query;
 
@@ -181,6 +198,7 @@
         });
     })
 
+    //Endpoint to update the title of a task
     app.get('/task/update-title', (req, res) => {
         const {id, title} = req.query;
 
@@ -195,6 +213,7 @@
         });
     })
 
+    //Endpoint to get a task by id and view attributes in JSON format
     app.get('/task/get-task', (req, res) => {
         const {id} = req.query;
 
