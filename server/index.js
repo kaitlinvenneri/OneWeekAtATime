94ab5656480ed557528b61447c30f3070c790bb2
Kaitlin <vennerik@uoguelph.ca>
2020-05-22 00:41:20 -0400

    Updating server for grading and fixing some bugs I found in testing

--- server/index.js
+++ server/index.js
@@ -9,11 +9,24 @@
         console.log("Listening on port 4000");
     })
 
+    let hostname = "localhost";
+    let username = "root";
+    let dbPassword = "password";
+    let dbName = "planningApp";
+
+    const args = process.argv.slice(2)
+    if(args[0] === 'grading'){
+        hostname = "dursley.socs.uoguelph.ca";
+        username = "vennerik";
+        dbPassword = "0885662";
+        dbName = "vennerik";
+    }
+
     var connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "planningApp"
+        host: hostname,
+        user: username,
+        password: dbPassword,
+        database: dbName
     });
 
     connection.connect(err => {
@@ -29,7 +42,7 @@
     })
 
     app.get('/tasks', (req, res) => {
-        const SELECT_ALL_TASKS_QUERY = `SELECT * FROM task`
+        const SELECT_ALL_TASKS_QUERY = `SELECT * FROM Task`
         
         connection.query(SELECT_ALL_TASKS_QUERY, (err, results) => {
             if(err) {
@@ -43,7 +56,7 @@
     })
 
     app.get('/scheduled-tasks', (req, res) => {
-        const SELECT_ALL_TASKS_QUERY = `SELECT * FROM scheduledtask`
+        const SELECT_ALL_TASKS_QUERY = `SELECT * FROM ScheduledTask`
 
         connection.query(SELECT_ALL_TASKS_QUERY, (err, results) => {
             if(err) {
@@ -59,7 +72,7 @@
     app.get('/task/add', (req, res) => {
         const {title} = req.query;
 
-        const INSERT_TASK_QUERY = `INSERT INTO task(title) VALUES('${title}')`
+        const INSERT_TASK_QUERY = `INSERT INTO Task(title) VALUES('${title}')`
 
         connection.query(INSERT_TASK_QUERY, (err) => {
             if(err) {
@@ -73,7 +86,7 @@
     app.get('/task/schedule', (req, res) => {
         const {id, date} = req.query;
 
-        const SCHEDULE_TASK_QUERY = `INSERT INTO scheduledtask(taskid,scheduleddate) VALUES('${id}', '${date}')`
+        const SCHEDULE_TASK_QUERY = `INSERT INTO ScheduledTask(taskid,scheduleddate) VALUES('${id}', '${date}')`
 
         connection.query(SCHEDULE_TASK_QUERY, (err) => {
             if(err) {
@@ -84,10 +97,10 @@
         });
     })
 
-    app.get('/task/mark-task-complete', (req, res) => {
+    app.get('/task/mark-complete', (req, res) => {
         const {id} = req.query;
 
-        const MARK_TASK_COMPLETE_QUERY = `UPDATE task SET completionstatus = '1' WHERE taskid = '${id}';`
+        const MARK_TASK_COMPLETE_QUERY = `UPDATE Task SET completionstatus = '1' WHERE taskid = '${id}';`
 
         connection.query(MARK_TASK_COMPLETE_QUERY, (err) => {
             if(err) {
@@ -98,10 +111,10 @@
         });
     })
 
-    app.get('/task/mark-scheduled-complete', (req, res) => {
+    app.get('/scheduled-task/mark-complete', (req, res) => {
         const {id} = req.query;
 
-        const MARK_SCHEDULED_COMPLETE_QUERY = `UPDATE scheduledtask SET completionstatus = '1' WHERE scheduledid = '${id}';`
+        const MARK_SCHEDULED_COMPLETE_QUERY = `UPDATE ScheduledTask SET completionstatus = '1' WHERE scheduledid = '${id}';`
 
         connection.query(MARK_SCHEDULED_COMPLETE_QUERY, (err) => {
             if(err) {
@@ -112,10 +125,10 @@
         });
     })
 
-    app.get('/task/mark-task-incomplete', (req, res) => {
+    app.get('/task/mark-incomplete', (req, res) => {
         const {id} = req.query;
 
-        const MARK_TASK_INCOMPLETE_QUERY = `UPDATE task SET completionstatus = '0' WHERE taskid = '${id}';`
+        const MARK_TASK_INCOMPLETE_QUERY = `UPDATE Task SET completionstatus = '0' WHERE taskid = '${id}';`
 
         connection.query(MARK_TASK_INCOMPLETE_QUERY, (err) => {
             if(err) {
@@ -126,10 +139,10 @@
         });
     })
 
-    app.get('/task/mark-scheduled-incomplete', (req, res) => {
+    app.get('/scheduled-task/mark-incomplete', (req, res) => {
         const {id} = req.query;
 
-        const MARK_SCHEDULED_INCOMPLETE_QUERY = `UPDATE scheduledtask SET completionstatus = '0' WHERE scheduledid = '${id}';`
+        const MARK_SCHEDULED_INCOMPLETE_QUERY = `UPDATE ScheduledTask SET completionstatus = '0' WHERE scheduledid = '${id}';`
 
         connection.query(MARK_SCHEDULED_INCOMPLETE_QUERY, (err) => {
             if(err) {
@@ -143,7 +156,7 @@
     app.get('/task/delete', (req, res) => {
         const {id} = req.query;
 
-        const DELETE_TASK_QUERY = `DELETE from task WHERE taskid = '${id}';`
+        const DELETE_TASK_QUERY = `DELETE from Task WHERE taskid = '${id}';`
 
         connection.query(DELETE_TASK_QUERY, (err) => {
             if(err) {
@@ -157,7 +170,7 @@
     app.get('/scheduled-task/delete', (req, res) => {
         const {id} = req.query;
 
-        const DELETE_SCHEDULED_TASK_QUERY = `DELETE from scheduledtask WHERE scheduledid = '${id}';`
+        const DELETE_SCHEDULED_TASK_QUERY = `DELETE from ScheduledTask WHERE scheduledid = '${id}';`
 
         connection.query(DELETE_SCHEDULED_TASK_QUERY, (err) => {
             if(err) {
@@ -171,7 +184,7 @@
     app.get('/task/update-title', (req, res) => {
         const {id, title} = req.query;
 
-        const UPDATE_TITLE_TASK_QUERY = `UPDATE task SET title = '${title}' WHERE taskid = '${id}';`
+        const UPDATE_TITLE_TASK_QUERY = `UPDATE Task SET title = '${title}' WHERE taskid = '${id}';`
 
         connection.query(UPDATE_TITLE_TASK_QUERY, (err) => {
             if(err) {
@@ -185,13 +198,13 @@
     app.get('/task/get-task', (req, res) => {
         const {id} = req.query;
 
-        const GET_TASK_QUERY = `SELECT * FROM task WHERE taskid = '${id}';`
+        const GET_TASK_QUERY = `SELECT * FROM Task WHERE taskid = '${id}';`
 
-        connection.query(GET_TASK_QUERY, (err) => {
+        connection.query(GET_TASK_QUERY, (err, results) => {
             if(err) {
                 return res.send(err)
             } else {
-                return res.send('successfully got task')
+                return res.json(results)
             }
         });
     })
