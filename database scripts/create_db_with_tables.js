098a79506e6ca658d61e9caefec5cbb83d05c357
Kaitlin <vennerik@uoguelph.ca>
2020-05-21 23:41:46 -0400

    Resolved issue so I can create db on socs server now

--- database scripts/create_db_with_tables.js
+++ database scripts/create_db_with_tables.js
@@ -3,12 +3,14 @@ var mysql = require('mysql');
 let hostname = "localhost";
 let username = "root";
 let dbPassword = "password";
+let dbName = "planningApp";
 
 const args = process.argv.slice(2)
 if(args[0] === 'grading'){
     hostname = "dursley.socs.uoguelph.ca";
     username = "vennerik";
     dbPassword = "0885662";
+    dbName = "vennerik";
 }
 
 var connection = mysql.createConnection({
@@ -21,17 +23,24 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    connection.query("CREATE DATABASE IF NOT EXISTS planningapp", function (err) {
+
+    const CREATE_DATABASE_QUERY = `CREATE DATABASE IF NOT EXISTS ${dbName};`
+
+    connection.query(CREATE_DATABASE_QUERY, function (err) {
         if (err) throw err;
         console.log("Database created");
     });
 
-    connection.query("CREATE TABLE IF NOT EXISTS planningapp.Task ( Taskid int NOT NULL AUTO_INCREMENT, Title varchar(255) NOT NULL, CompletionStatus int DEFAULT 0, PRIMARY KEY (Taskid))", function (err) {
+    const CREATE_TASK_TABLE_QUERY = `CREATE TABLE IF NOT EXISTS ${dbName}.Task ( Taskid int NOT NULL AUTO_INCREMENT, Title varchar(255) NOT NULL, CompletionStatus int DEFAULT 0, PRIMARY KEY (Taskid));`
+
+    connection.query(CREATE_TASK_TABLE_QUERY, function (err) {
         if (err) throw err;
         console.log("Task Table created");
     });
 
-    connection.query("CREATE TABLE IF NOT EXISTS planningapp.ScheduledTask ( Scheduledid int NOT NULL AUTO_INCREMENT, Taskid int, ScheduledDate date DEFAULT null, CompletionStatus int DEFAULT 0, PRIMARY KEY (Scheduledid), FOREIGN KEY (Taskid) REFERENCES planningapp.task(taskid) ON DELETE CASCADE)", function (err) {
+    CREATE_SCHEDULED_TASK_TABLE_QUERY = `CREATE TABLE IF NOT EXISTS ${dbName}.ScheduledTask ( Scheduledid int NOT NULL AUTO_INCREMENT, Taskid int NOT NULL, ScheduledDate date DEFAULT null, CompletionStatus int DEFAULT 0, PRIMARY KEY (Scheduledid), FOREIGN KEY (Taskid) REFERENCES ${dbName}.Task(Taskid) ON DELETE CASCADE);`
+
+    connection.query(CREATE_SCHEDULED_TASK_TABLE_QUERY, function (err) {
         if (err) throw err;
         console.log("Scheduled Task Table created");
     });
