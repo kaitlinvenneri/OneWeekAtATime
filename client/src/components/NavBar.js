105b4a5d62f9fc34adce794162b2344dcb226ba8
Kaitlin <vennerik@uoguelph.ca>
2020-07-03 23:03:37 -0400

    Removing links to other pages for now

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,28 +11,6 @@ class NavBar extends Component {
         <a className="navbar-brand" href="#">
           Planning App
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        ></button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-item nav-link active" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-            <a className="nav-item nav-link" href="#">
-              Features
-            </a>
-            <a className="nav-item nav-link" href="#">
-              Pricing
-            </a>
-          </div>
-        </div>
       </nav>
     );
   }
