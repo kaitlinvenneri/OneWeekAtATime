1af9306b55c33058a7e5011ed44662701ef21c0e
Kaitlin <vennerik@uoguelph.ca>
2020-07-03 23:51:10 -0400

    Adding more icons to WeekView and WeekViewTasks

--- client/src/components/WeekViewTask.js
+++ client/src/components/WeekViewTask.js
@@ -52,6 +52,7 @@ class WeekViewTask extends Component {
               fill="currentColor"
               xmlns="http://www.w3.org/2000/svg"
               onClick={this.changeSelectedState}
+              style={{ cursor: "pointer" }}
             >
               <path
                 fillRule="evenodd"
@@ -67,6 +68,7 @@ class WeekViewTask extends Component {
               fill="currentColor"
               xmlns="http://www.w3.org/2000/svg"
               onClick={this.changeSelectedState}
+              style={{ cursor: "pointer" }}
             >
               <path
                 fillRule="evenodd"
@@ -78,20 +80,39 @@ class WeekViewTask extends Component {
               />
             </svg>
           )}
-
           <svg
             width="1.25em"
             height="1.25em"
             viewBox="0 0 16 16"
-            className="bi bi-x-square-fill mb-2"
+            className="bi bi-x-square-fill mb-1"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
             onClick={() => onDelete(task.scheduledId, task.taskId)}
+            style={{ cursor: "pointer" }}
           >
             <path
               fillRule="evenodd"
               d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm9.854 4.854a.5.5 0 0 0-.708-.708L8 7.293 4.854 4.146a.5.5 0 1 0-.708.708L7.293 8l-3.147 3.146a.5.5 0 0 0 .708.708L8 8.707l3.146 3.147a.5.5 0 0 0 .708-.708L8.707 8l3.147-3.146z"
             />
+          </svg>
+          <svg
+            width="1.25em"
+            height="1.25em"
+            viewBox="0 0 16 16"
+            class="bi bi-calendar-event mb-2"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ cursor: "pointer" }}
+          >
+            <path
+              fill-rule="evenodd"
+              d="M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1zm1-3a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M3.5 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zm9 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z"
+            />
+            <rect width="2" height="2" x="11" y="6" rx=".5" />
           </svg>
         </div>
         <label
