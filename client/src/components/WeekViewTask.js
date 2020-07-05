7145b4acd868519b4146ff35391ad1f21d3482b6
Kaitlin <vennerik@uoguelph.ca>
2020-07-04 23:37:33 -0400

    Getting rid of errors re. className and fillRule

--- client/src/components/WeekViewTask.js
+++ client/src/components/WeekViewTask.js
@@ -120,17 +120,17 @@ class WeekViewTask extends Component {
             width="1.25em"
             height="1.25em"
             viewBox="0 0 16 16"
-            class="bi bi-calendar-event mb-2"
+            className="bi bi-calendar-event mb-2"
             fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
             style={{ cursor: "pointer" }}
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1zm1-3a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z"
             />
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3.5 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zm9 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z"
             />
             <rect width="2" height="2" x="11" y="6" rx=".5" />
