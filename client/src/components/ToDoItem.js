64d1b6b6f2f755aeea8deed929cc3af259939e95
Kaitlin <vennerik@uoguelph.ca>
2020-07-08 12:37:55 -0400

    Factoring out the svgs from ToDoList and ToDoItem

--- client/src/components/ToDoItem.js
+++ client/src/components/ToDoItem.js
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import TodoChecked from "./../svgs/TodoChecked";
+import TodoUnchecked from "./../svgs/TodoUnchecked";
+import TodoEdit from "./../svgs/TodoEdit";
+import TodoSchedule from "./../svgs/TodoSchedule";
+import TodoViewScheduled from "./../svgs/TodoViewScheduled";
+import TodoDelete from "../svgs/TodoDelete";
 
 class ToDoItem extends Component {
   state = {};
@@ -18,41 +24,9 @@ class ToDoItem extends Component {
         <div className="d-inline-flex flex-row align-items-center">
           <div>
             {task.completionStatus === 1 ? (
-              <svg
-                width="1.25em"
-                height="1.25em"
-                viewBox="0 0 16 16"
-                className="bi bi-check-square-fill mr-2"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                style={{ cursor: "pointer" }}
-                onClick={() => onChangeCompletion(task)}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"
-                />
-              </svg>
+              <TodoChecked onClick={onChangeCompletion} task={task} />
             ) : (
-              <svg
-                width="1.25em"
-                height="1.25em"
-                viewBox="0 0 16 16"
-                className="bi bi-check-square mr-2"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                style={{ cursor: "pointer" }}
-                onClick={() => onChangeCompletion(task)}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"
-                />
-              </svg>
+              <TodoUnchecked onClick={onChangeCompletion} task={task} />
             )}
           </div>
           <div className="mt-1" style={labelStyle}>
@@ -64,92 +38,13 @@ class ToDoItem extends Component {
             <></>
           ) : (
             <div>
-              <svg
-                width="1.3em"
-                height="1.3em"
-                viewBox="0 0 16 16"
-                className="bi bi-pencil-square mr-1"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                style={{ cursor: "pointer" }}
-              >
-                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                <path
-                  fillRule="evenodd"
-                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                />
-              </svg>
-              <svg
-                width="1.25em"
-                height="1.25em"
-                viewBox="0 0 16 16"
-                className="bi bi-calendar-plus mx-1"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                style={{ cursor: "pointer" }}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M7.5 9.5A.5.5 0 0 1 8 9h2a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0v-2z"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1zm1-3a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M3.5 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zm9 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z"
-                />
-              </svg>
-              <svg
-                width="1.25em"
-                height="1.25em"
-                viewBox="0 0 16 16"
-                className="bi bi-calendar-week mx-1"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                style={{ cursor: "pointer" }}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1zm1-3a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M3.5 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zm9 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z"
-                />
-                <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
-              </svg>
+              <TodoEdit />
+              <TodoSchedule />
+              <TodoViewScheduled />
             </div>
           )}
           <div>
-            <svg
-              width="1.25em"
-              height="1.25em"
-              viewBox="0 0 16 16"
-              className="bi bi-x-square ml-1"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-              style={{ cursor: "pointer" }}
-              onClick={() => onDelete(task.taskId)}
-            >
-              <path
-                fillRule="evenodd"
-                d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
-              />
-              <path
-                fillRule="evenodd"
-                d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"
-              />
-              <path
-                fillRule="evenodd"
-                d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"
-              />
-            </svg>
+            <TodoDelete onDelete={onDelete} task={task} />
           </div>
         </div>
       </div>
