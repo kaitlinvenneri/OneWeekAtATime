d9591fedee04c3805923c3169599ffd1ba80af13
Kaitlin <vennerik@uoguelph.ca>
2020-07-09 02:21:26 -0400

    Changing svg to trash for delete task

--- client/src/svgs/TodoDelete.js
+++ client/src/svgs/TodoDelete.js
@@ -5,26 +5,19 @@ class TodoDelete extends Component {
     const { editing, onDelete, task } = this.props;
     return (
       <svg
-        width="1.25em"
-        height="1.25em"
+        width="1.35em"
+        height="1.35em"
         viewBox="0 0 16 16"
-        className="bi bi-x-square ml-1"
+        className="bi bi-trash"
         fill={editing === true ? 'grey' : 'currentColor'}
         xmlns="http://www.w3.org/2000/svg"
         style={editing === true ? {} : { cursor: 'pointer' }}
         onClick={editing === true ? null : () => onDelete(task.taskId)}
       >
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
         <path
           fillRule="evenodd"
-          d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
-        />
-        <path
-          fillRule="evenodd"
-          d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"
-        />
-        <path
-          fillRule="evenodd"
-          d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"
+          d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
         />
       </svg>
     );
