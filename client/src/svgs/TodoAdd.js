7270c1484b2e80c3cf9c02bf1cd09a691be41c65
Kaitlin <vennerik@uoguelph.ca>
2020-07-11 16:11:29 -0400

    Changing the svg size for the add todo to look nicer

--- client/src/svgs/TodoAdd.js
+++ client/src/svgs/TodoAdd.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class TodoAdd extends Component {
   render() {
     return (
       <svg
-        width="2em"
-        height="2em"
+        width="1.7em"
+        height="1.7em"
         viewBox="0 0 16 16"
         className="bi bi-journal-plus"
         fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
-        style={{ cursor: "pointer" }}
+        style={{ cursor: 'pointer' }}
       >
         <path d="M4 1h5v1H4a1 1 0 0 0-1 1H2a2 2 0 0 1 2-2zm10 7v5a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2h1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V8h1zM2 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2z" />
         <path
