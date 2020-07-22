05ca7b8feb81132b2a20930b2d034831270924a5
Kaitlin <vennerik@uoguelph.ca>
2020-07-21 23:26:54 -0400

    Remove reschedule svg from WeekView tasks

--- client/src/components/WeekViewTask.js
+++ client/src/components/WeekViewTask.js
@@ -20,9 +20,15 @@ class WeekViewTask extends Component {
     };
 
     if (task.completionStatus === 1) {
-      await axios.get('http://localhost:4000/scheduled-task/mark-complete', options);
+      await axios.get(
+        'http://localhost:4000/scheduled-task/mark-complete',
+        options
+      );
     } else {
-      await axios.get('http://localhost:4000/scheduled-task/mark-incomplete', options);
+      await axios.get(
+        'http://localhost:4000/scheduled-task/mark-incomplete',
+        options
+      );
     }
   };
 
@@ -97,25 +103,6 @@ class WeekViewTask extends Component {
           </label>
         </div>
         <div className="d-flex flex-row justify-content-end mb-2">
-          <svg
-            width="1.2em"
-            height="1.2em"
-            viewBox="0 0 16 16"
-            className="bi bi-calendar-week mr-1"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{ cursor: 'pointer' }}
-          >
-            <path
-              fillRule="evenodd"
-              d="M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1zm1-3a2 2 0 0 0-2 2v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2z"
-            />
-            <path
-              fillRule="evenodd"
-              d="M3.5 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zm9 0a.5.5 0 0 1 .5.5V1a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5z"
-            />
-            <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
-          </svg>
           <svg
             width="1.25em"
             height="1.25em"
