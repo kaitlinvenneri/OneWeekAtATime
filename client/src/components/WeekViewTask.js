1d23ab3c16ceeddb06a3b33f66562ed173c7bca2
Kaitlin <vennerik@uoguelph.ca>
2020-08-03 19:09:29 -0400

    Factoring out svgs

--- client/src/components/WeekViewTask.js
+++ client/src/components/WeekViewTask.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import WeekViewTaskChecked from './../svgs/WeekViewTaskChecked';
+import WeekViewTaskUnchecked from '../svgs/WeekViewTaskUnchecked';
+import WeekViewTaskDelete from '../svgs/WeekViewTaskDelete';
 
 class WeekViewTask extends Component {
   state = { completionStatus: this.props.task.completionStatus };
@@ -57,41 +60,9 @@ class WeekViewTask extends Component {
         <div className="d-flex flex-row">
           <div>
             {this.state.completionStatus === 0 ? (
-              <svg
-                width="1.1em"
-                height="1.1em"
-                viewBox="0 0 16 16"
-                className="bi bi-check-square mr-2"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                onClick={this.changeSelectedState}
-                style={{ cursor: 'pointer' }}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"
-                />
-              </svg>
+              <WeekViewTaskUnchecked onClick={this.changeSelectedState} />
             ) : (
-              <svg
-                width="1.1em"
-                height="1.1em"
-                viewBox="0 0 16 16"
-                className="bi bi-check-square-fill mr-2"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                onClick={this.changeSelectedState}
-                style={{ cursor: 'pointer' }}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"
-                />
-              </svg>
+              <WeekViewTaskChecked onClick={this.changeSelectedState} />
             )}
           </div>
           <label
@@ -103,22 +74,7 @@ class WeekViewTask extends Component {
           </label>
         </div>
         <div className="d-flex flex-row justify-content-end mb-2">
-          <svg
-            width="1.25em"
-            height="1.25em"
-            viewBox="0 0 16 16"
-            className="bi bi-trash"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={() => onDelete(task.scheduledId, task.taskId)}
-            style={{ cursor: 'pointer' }}
-          >
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-            <path
-              fillRule="evenodd"
-              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-            />
-          </svg>
+          <WeekViewTaskDelete task={task} onClick={onDelete} />
         </div>
       </div>
     );
---
+++ client/src/svgs/WeekViewTaskUnchecked.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class WeekViewTaskUnchecked extends Component {
+  render() {
+    const { onClick } = this.props;
+
+    return (
+      <svg
+        width="1.1em"
+        height="1.1em"
+        viewBox="0 0 16 16"
+        className="bi bi-check-square mr-2"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        onClick={onClick}
+        style={{ cursor: 'pointer' }}
+      >
+        <path
+          fillRule="evenodd"
+          d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+        />
+        <path
+          fillRule="evenodd"
+          d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"
+        />
+      </svg>
+    );
+  }
+}
+
+export default WeekViewTaskUnchecked;
