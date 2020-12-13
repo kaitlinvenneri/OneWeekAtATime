34aa385505517736471fdcd25c776dd34ad81d8a
Kaitlin <vennerik@uoguelph.ca>
2020-12-13 01:23:58 -0500

    Fixing error

--- client/src/svgs/MenuLines.js
+++ client/src/svgs/MenuLines.js
@@ -16,7 +16,7 @@ class MenuLines extends Component {
         onClick={onClick}
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
         />
       </svg>
