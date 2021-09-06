8f5c6a86421e5cf6c8f399a555a06f3822211601
Kaitlin <kaitlinvenneri@outlook.com>
2021-09-06 17:28:20 -0400

    Category colors now exist in a map, all in one place

---
+++ client/src/styling/CategoryColors.js
@@ -0,0 +1,7 @@
+export const categoryColors = new Map([
+  ['gray', { backgroundColor: '#e1e6ea', borderColor: '#4b5968' }],
+  ['blue', { backgroundColor: '#e3f2fd', borderColor: '#0c66a6' }],
+  ['pink', { backgroundColor: '#ffcce6', borderColor: '#cc0069' }],
+  ['green', { backgroundColor: '#b3e6b3', borderColor: '#194d33' }],
+  ['orange', { backgroundColor: '#ffe0b3', borderColor: '#e65c00' }],
+]);
