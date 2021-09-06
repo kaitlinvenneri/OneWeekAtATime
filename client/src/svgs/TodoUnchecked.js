efd1a8d4d53eb2e39da058008c9a524b6104a47d
Kaitlin <kaitlinvenneri@outlook.com>
2021-09-06 16:22:08 -0400

    let's see what's wrong

--- client/src/svgs/TodoUnchecked.js
+++ client/src/svgs/TodoUnchecked.js
@@ -2,41 +2,20 @@ import React, { Component } from 'react';
 
 class TodoUnchecked extends Component {
   render() {
-    const { editing, scheduling, onClick, task } = this.props;
-
-    let classString = '';
-
-    if (editing) {
-      classString = 'bi bi-check-square mr-1';
-    } else {
-      classString = 'bi bi-check-square mr-2';
-    }
+    const { onClick, task } = this.props;
 
     return (
       <svg
         width="1em"
         height="1em"
         viewBox="0 0 16 16"
-        className={classString}
-        fill={
-          editing === false && scheduling === false ? 'currentColor' : 'grey'
-        }
+        className="bi bi-square mr-2"
+        fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
-        style={
-          editing === false && scheduling === false ? { cursor: 'pointer' } : {}
-        }
-        onClick={
-          editing === false && scheduling === false ? () => onClick(task) : null
-        }
+        style={{ cursor: 'pointer' }}
+        onClick={() => onClick(task)}
       >
-        <path
-          fillRule="evenodd"
-          d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
-        />
-        <path
-          fillRule="evenodd"
-          d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"
-        />
+        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
       </svg>
     );
   }
